c8288b8375e179085b51bd4c946ee1088958f1cc
Stefano <stefano@stefano.com>
2025-07-15 19:14:25 -0300

    Estructura docker y dockerfile actualizada

--- UsersApp/urls.py
+++ UsersApp/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenRefreshView
 from .views import (
     RegisterView, 
     VerifyTokenView, 
@@ -14,8 +13,6 @@ urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('verify/', VerifyTokenView.as_view(), name='verify'),
     path('login/', LoginView.as_view(), name='login'),
-    
-    # Endpoints para gestión de sesión
     path('token/refresh/', CustomTokenRefreshView.as_view(), name='token_refresh'),
     path('session/extend/', SessionExtendView.as_view(), name='session_extend'),
     path('session/heartbeat/', ActivityHeartbeatView.as_view(), name='activity_heartbeat'),
